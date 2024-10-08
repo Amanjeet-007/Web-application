0d9d8c178104ac2d434af0e21f36b6fd0038271a
Amanjeet Kumar <kumaramanjeet951@gmail.com>
2024-10-08 15:20:30 +0530

    second change

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
 router.get('/',(req, res, next)=> {
   res.render('index', { title: 'Application' });
@@ -9,10 +10,13 @@ router.get('/about',(req,res,next)=>{
   res.render('about',{ title: 'Application' })
 })
 router.get('/project',(req,res,next)=>{
-  res.render('project')
+  res.render('project',{ title: 'Projects' })
 })
 router.get('/project/javascript',(req,res,next)=>{
-  res.render('javascript')
+  res.render('javascript',{ title: 'JavaScript' })
+})
+router.get('/project/java',(req,res,next)=>{
+  res.render('java',{ title: 'Java' })
 })
 
 module.exports = router;
